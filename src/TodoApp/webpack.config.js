633a657c84152e1af9bab906b89ea55fae896d2c
martincostello <martin@martincostello.com>
2024-05-19 08:57:36 +0100

    Update obsolete linter rule
    - Remove obsolete ESLint rule.
    - Fix lint violations in webpack configuration.

--- src/TodoApp/webpack.config.js
+++ src/TodoApp/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
+const cssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const removeEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 
 module.exports = {
     devtool: 'source-map',
@@ -16,8 +16,8 @@ module.exports = {
             {
                 test: /.css$/,
                 use: [
-                  MiniCssExtractPlugin.loader,
-                  { loader: "css-loader", options: { sourceMap: true } },
+                    miniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { sourceMap: true } },
                 ],
             },
             {
@@ -30,8 +30,8 @@ module.exports = {
     optimization: {
         minimize: true,
         minimizer: [
-            `...`,
-            new CssMinimizerPlugin(),
+            '...',
+            new cssMinimizerPlugin(),
         ],
     },
     output: {
@@ -39,10 +39,10 @@ module.exports = {
         path: path.resolve(__dirname, 'wwwroot', 'static'),
     },
     plugins: [
-        new MiniCssExtractPlugin({
+        new miniCssExtractPlugin({
             filename: '[name]/main.css'
         }),
-        new RemoveEmptyScriptsPlugin(),
+        new removeEmptyScriptsPlugin(),
         new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en-gb/),
     ],
     resolve: {
